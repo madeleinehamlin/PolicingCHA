af60ac9bcfd5f3a7552ab4a7572ca8d3288cba06
Madeleine Hamlin <maddy.hamlin@gmail.com>
2024-01-12 19:50:46 -0500

    Maddy edits Jan12

--- storytelling-main/src/config.js
+++ storytelling-main/src/config.js
@@ -8,18 +8,15 @@ var config = {
     use3dTerrain: false,
     auto: false,
     title: 'POLICING CHICAGO PUBLIC HOUSING',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
     footer: 'Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'Section1',
-            alignment: 'center',
+            alignment: 'right',
             hidden: false,
             image: 'images/Image1.png',
             description: 'Chicago is one of the most racially segregated cities in the country. This pattern is not an accident nor is it the result of individual preferences.<br><br>',
-            permissions: 'Permissions: made by Maddy & Maheen<br>',
-            credit: 'Credits: Map made by Madeleine Hamlin and Maheen Khan',
+            credit: 'Map made by Madeleine Hamlin and Maheen Khan using data from the 2020 U.S. Census',
             location: {
                 center: [-87.63726, 41.88938],
                 zoom: 12,
@@ -31,12 +28,11 @@ var config = {
         },
         {
             id: 'Section2',
-            alignment: 'center',
+            alignment: 'right',
             hidden: false,
             image: 'images/Image2.jpeg',
             description: 'Between 1916 and 1970, 500,000 African Americans moved to Chicago from the South during successive waves of the Great Migration. Many sought a better life than they had experienced as sharecroppers in the South.<br><br>',
-            permissions: "Permissions: public domain<br>",
-            credit: "Credits: Library of Congress",
+            credit: "Image source: W.E.B. DuBois, The Georgia Negro Migration of Negroes, ca. 1900, Library of Congress",
             location: {
                 center: [-87.63726, 41.88938],
                 zoom: 12,
@@ -51,11 +47,11 @@ var config = {
             alignment: 'right',
             hidden: false,
             image: 'images/Image3.webp',
-            description: 'The influx of newcomers was largely funneled into an area that became known as the “Black Belt”— a narrow 40-block-long strip on the city’s South Side.',
-            permissions: "Permissions: have sent multiple emails to WTTW permissions contact with no response",
+            description: 'The influx of newcomers was largely funneled into an area that became known as the “Black Belt”— a narrow 40-block-long strip on the city’s South Side.<br><br>',
+			credit: "Image source: WTTW",
             location: {
-                center: [-87.60742, 41.8172],
-                zoom: 13.15,
+                center: [-87.63726, 41.88938],
+                zoom: 12,
                 pitch: 0,
                 bearing: 0
             },
@@ -68,12 +64,11 @@ var config = {
             hidden: false,
             image: 'images/Image4.jpg',
             description: 'The Great Migration gave rise to tensions over housing and employment that erupted into violence in the 1919 Riot. The riot set the borders of the Black Belt. Prior to the riot, the city was not yet rigidly segregated. After the riot, the boundaries between Black and white Chicago were sharply drawn and fiercely defended. Historian Adam Green has observed that police responded to the public emergency of the riot by establishing a cordon around the Black Belt—the “first citywide example of differential policing.” An emergency measure in the midst of the race war, policing as containment soon became normalized.<br><br>',
-            permissions: "Permissions: permission obtained from Chicago History Museum<br>",
-            credit: "Credits: Chicago History Museum, ICHi-065485; Jun Fujita, photographer",
+            credit: "Image source: Chicago History Museum, ICHi-065485; Jun Fujita, photographer",
             location: {
                 center: [-87.60742, 41.8172],
                 zoom: 13.15,
-                pitch: 0,
+                pitch: 45,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
@@ -85,12 +80,11 @@ var config = {
             hidden: false,
             image: 'images/Image5.jpeg',
             description: 'Under conditions of rigid segregation, the Black Belt comprised a city-within-a-city, a site of cultural and entrepreneurial vitality. At the same time, it was a ghettoized containment zone. Although Black workers were integral to the city’s economy, they were prevented from meaningful social or economic mixing with other groups. And most residents of the Black Belt were forced to live in conditions of stark poverty, in degraded and unsafe tenements where they were exposed to environmental toxins, disease, and rodent infestations<br><br>',
-            permissions: "Permissions: public domain<br>",
-            credit: "Credits: Library of Congress",
+            credit: "Image source: Library of Congress",
             location: {
                 center: [-87.60742, 41.8172],
                 zoom: 13.15,
-                pitch: 0,
+                pitch: 45,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
@@ -102,12 +96,11 @@ var config = {
             hidden: false,
             image: 'images/Image6.jpg',
             description: 'In the 1930s, progressive housing reformers pushed to tear down the tenements and rebuild Chicago’s slum neighborhoods. Federal funding established the Chicago Housing Authority (CHA), which, like similar agencies across the nation, began a widespread construction program intended to house the city’s poor in new, modern structures where, it was believed, they would experience "social rebirth."<br><br>',
-            permissions: "Permissions: permission obtained from Chicago History Museum<br>",
-            credit: "Credits: Chicago History Museum, ICHi-035845; Clarence W. Hines, photographer",
+            credit: "Image source: Chicago History Museum, ICHi-035845; Clarence W. Hines, photographer",
             location: {
                 center: [-87.60742, 41.8172],
                 zoom: 13.15,
-                pitch: 0,
+                pitch: 45,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
@@ -121,7 +114,7 @@ var config = {
             location: {
                 center: [-87.60742, 41.8172],
                 zoom: 13.15,
-                pitch: 0,
+                pitch: 45,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
@@ -129,17 +122,16 @@ var config = {
         },
         {
             id: 'Section8',
-            alignment: 'right',
+            alignment: 'left',
             hidden: false,
             image: 'images/Image8.jpg',
             description: 'When early developments—such as the Airport Homes and Trumbull Park Homes—began to integrate in the late 1940s and 1950s, massive race riots broke out. CHA called in the police to quell tensions, but they proved incapable of managing the hostilities. Even after 700 police officers were stationed for months at the Trumbull Park site, the first Black family to move in gave up and moved out. Although the police were ineffective at promoting integration, these events solidified the relationship between the CHA and local police forces. Going forward, police were seen as the central tool for managing and controlling public housing residents.<br><br>',
-            permissions: "Permissions: permission obtained from Chicago History Museum<br>",
-            credit: "Credits: Chicago History Museum, ICHi-036817",
+            credit: "Image source: Chicago History Museum, ICHi-036817",
             location: {
-                center: [-87.56371, 41.70226],
-                zoom: 14.5,
-                pitch: 0,
-                bearing: 0
+                center: [-87.56585, 41.70006],
+                zoom: 16.5,
+                pitch: 70,
+                bearing: -30
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -192,8 +184,7 @@ var config = {
             hidden: false,
             image: 'images/Image12.png',
             description: 'In the 1990s, in part in response to complaints that the CPD was failing to adequately police public housing, the it was joined by a new force: the Chicago Housing Authority Police. Building an in-house police force expanded CHA’s law enforcement function, making it both landlord and police. CHA Police officers—numbering about 500 at the force’s height—had full powers of arrest in CHA developments and worked out of sub-stations within several of the complexes, complete with lock-up facilities. Together, CPD and the CHA police officers conducted unconstitutional raids of public housing buildings, ransacking apartments before dawn. In short, the CHA Police force drained federal monies otherwise earmarked for building maintenance, while making public housing into an ever-more prison-like space—one where all residents were treated as criminals and presumed guilty until proven innocent.<br><br>',
-            permissions: "Permissions: Patricia Evans<br>",
-            credit: "Credits: (c) Patricia Evans",
+            credit: "Photo (c) Patricia Evans",
             location: {
                 center: [-87.64092, 41.90058],
                 zoom: 17,
@@ -238,8 +229,7 @@ var config = {
             hidden: false,
             image: 'images/image15.png',
             description: 'Long before the Plan for Transformation was announced, it was clear to residents that demolition was coming. CHA ceased turning over vacant units, slowing shutting down entire floors and then entire buildings. Thus, the resident population began to decline, despite the massive demand for housing as reflected in the city’s decades-long public housing waitlist. This artificial production of vacancy was used by CHA as a rationale for reducing the number of units it was responsible for replacing—from 42,000 units at CHA’s height, to the 25,000 units that were promised in the Plan. Some high-rises became de facto homeless shelters, as homeless families squatted in vacant units until evicted—a clear reflection of human need that was being unaddressed.<br><br>',
-            permissions: "Permissions: Patricia Evans<br>",
-            credit: "Credits: (c) Patricia Evans",
+            credit: "Photo (c) Patricia Evans",
             location: {
                 center: [-87.65909, 41.86522],
                 zoom: 14.5,
@@ -284,8 +274,7 @@ var config = {
             hidden: false,
             image: 'images/image18.jpg',
             description: 'Judged on its own terms, the Plan for Transformation did not achieve its stated goals. It is yet to produce the total number of replacement units CHA committed to produce. It has not resulted in thriving mixed income communities. In the course of forced relocation, the agency lost track of an unknown but substantial number of tenants. Today, fewer than 7,000 units of traditional public housing remain, while some 44,000 people are on the CHA waitlist for public housing and another 35,000 people are on the voucher waitlist. Large expanses of land that used to house those in need stand vacant in the central city; others, such as the former site of Cabrini Green, have been gentrified. And, the CHA has, in effect, become a commercial real estate broker for big box stores and sports facilities, including the XS Tennis Village at the site of the Robert Taylor Homes, Mariano’s at the site of the Ida B. Wells Homes, Target at the site of the Cabrini Green Homes, and a planned training facility for the Chicago Fire professional soccer team on the site of the ABLA Homes.<br><br>', 
-            permissions: 'Permissions: Patricia Evans<br>',
-            credit: "Credits: (c) Patricia Evans",
+            credit: "Photo (c) Patricia Evans",
             location: {
                 center: [-87.64986, 41.86878],
                 zoom: 12,
@@ -301,8 +290,7 @@ var config = {
             hidden: false,
             image: 'images/image19.png',
             description: 'There has been, however, one way in which the Plan deserves to be described as transformative. Demolition and forced relocation have effectively disappeared people, places, and issues. The Plan has also remapped our moral imaginations, shaping what we can see and what is rendered invisible. The upshot is that we are yet to reckon with this history and its implications for the future. Photo copyright Patricia Evans.<br><br>', 
-            permissions: 'Permissions: Patricia Evans<br>',
-            credit: "Credits: (c) Patricia Evans",
+            credit: "Photo (c) Patricia Evans",
             location: {
                 center: [-87.62610, 41.80931],
                 zoom: 15.5,
@@ -316,15 +304,15 @@ var config = {
             id: 'Section20',
             alignment: 'center',
             hidden: false,
-            description: 'This storymap is the product of a collaboration between Madeleine Hamlin, PhD, and the Invisible Institute. It was produced by Madeleine Hamlin and Jamie Kalven, with mapping and technical support from Tingkuan Hsieh, Maheen Khan, Sophie Schadler, and Sophie Thompson. Funding support for this project was provided by the Colgate University Public Affairs and Policy Research Initiative. Copyright 2023.', 
+            description: 'This storymap is the product of a collaboration between Madeleine Hamlin, PhD, and the Invisible Institute. It was produced by Madeleine Hamlin and Jamie Kalven, with mapping and technical support from Tingkuan Hsieh, Maheen Khan, Sophie Schadler, and Sophie Thompson. Funding support for this project was provided by the Colgate University Public Affairs and Policy Research Initiative. Copyright 2024.', 
             location: {
-                center: [-87.62610, 41.80931],
-                zoom: 15.5,
+                center: [-87.63726, 41.88938],
+                zoom: 12,
                 pitch: 0,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
-        }
+        },
     ]
 }
